ce6b80427695807c1a2cb03f86be1cd3a955f013
risako070310 <risako070310@gmail.com>
2021-05-09 16:46:09 +0900

    create count app

--- app/src/main/java/app/myoji/nickname/appjam/MainActivity.kt
+++ app/src/main/java/app/myoji/nickname/appjam/MainActivity.kt
@@ -2,6 +2,7 @@ package app.myoji.nickname.appjam
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +10,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var count = 0
+
+        plusButton.setOnClickListener {
+            count++
+            text.text = count.toString()
+        }
+
+        minusButton.setOnClickListener {
+            if(count > 0){
+                count--
+            }
+            text.text = count.toString()
+        }
+
+        clearButton.setOnClickListener {
+            count = 0
+            text.text = count.toString()
+        }
     }
 
 }
