c8dab24d111ef56a7af184fffa2a6d42c0e0358b
risako070310 <risako070310@gmail.com>
2021-05-15 09:18:54 +0900

    update original app

--- app/src/main/java/app/myoji/nickname/appjam/MainActivity.kt
+++ app/src/main/java/app/myoji/nickname/appjam/MainActivity.kt
@@ -1,33 +1,71 @@
 package app.myoji.nickname.appjam
 
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private var number = Random.nextInt(11)
+    private var count = 0
+
+    private var btn = listOf<Button>()
+
+    val handler = Handler(Looper.getMainLooper())
+    val run = object : Runnable{
+        override fun run() {
+            resetColor()
+            randomColor()
+            when {
+                count < 10 -> {
+                    handler.postDelayed(this, 1000)
+                }
+                count < 20 -> {
+                    handler.postDelayed(this, 800)
+                }
+                else -> {
+                    handler.postDelayed(this, 500)
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var count = 0
+        handler.post(run)
 
-        plusButton.setOnClickListener {
-            count++
-            text.text = count.toString()
-        }
+        btn = listOf(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12)
 
-        minusButton.setOnClickListener {
-            if(count > 0){
-                count--
+        btn.forEachIndexed { index, btn ->
+            btn.setOnClickListener {
+                checkColor(index)
             }
-            text.text = count.toString()
-        }
-
-        clearButton.setOnClickListener {
-            count = 0
-            text.text = count.toString()
         }
     }
 
+    private fun checkColor(button: Int){
+        if(number == button){
+            count++
+            text.text = count.toString()
+            resetColor()
+        }
+    }
+
+    private fun randomColor(){
+        number = Random.nextInt(11)
+        btn[number].setBackgroundColor(Color.parseColor("#03dac6"))
+    }
+
+    private fun resetColor(){
+        for(i in btn){
+            i.setBackgroundColor(Color.parseColor("#aaaaaa"))
+        }
+    }
 }
